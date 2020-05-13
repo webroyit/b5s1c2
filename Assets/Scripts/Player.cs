5e6be69d6f45a05fd5c1ecbeffc9f3e0d99fe514
royweb <xytank@gmail.com>
2020-05-13 13:16:45 -0400

    Use Input.GetAxisRaw() to get the direction of the player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,16 @@ public class Player : MonoBehaviour
     // Player speed
     public float speed = 4f;
 
+    // Access Rigidbody2D property
+    public Rigidbody2D rb;
+
+    private float movement = 0f;
+
     // Update is called once per frame
     void Update()
     {
-        
+        // Represents the direction that the player move
+        // It between -1 to 1
+        movement = Input.GetAxisRaw("Horizontal") * speed;
     }
 }
