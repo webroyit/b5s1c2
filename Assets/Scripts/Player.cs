3f105b6bd03c6c32593731a891f7aba6619cee65
royweb <xytank@gmail.com>
2020-05-13 12:42:17 -0400

    Add a class called Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Player speed
+    public float speed = 4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
