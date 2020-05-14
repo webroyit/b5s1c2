12602a645e05165bde7581477fd8214f4e15d2c5
royweb <xytank@gmail.com>
2020-05-14 13:27:45 -0400

    Add a class called Chain

---
+++ Assets/Scripts/Chain.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Chain : MonoBehaviour
+{
+    // static means that it can be access from anywhere without needing a reference to the instance of the class, but only the type
+    public static bool isFired;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isFired = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
