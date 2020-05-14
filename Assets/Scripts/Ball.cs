9b302a0945b2ba4145230f80221c9041715c2495
royweb <xytank@gmail.com>
2020-05-14 12:28:42 -0400

    Add a class called Ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Vector2 start;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
