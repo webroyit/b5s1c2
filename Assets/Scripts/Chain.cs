cfd8e70115b53537c1b66870da768aac433cff70
royweb <xytank@gmail.com>
2020-05-14 14:12:44 -0400

    Move the chain based on the player position

--- Assets/Scripts/Chain.cs
+++ Assets/Scripts/Chain.cs
@@ -2,6 +2,9 @@
 
 public class Chain : MonoBehaviour
 {
+    // Access Transform property on unity
+    public Transform player;
+
     // static means that it can be access from anywhere without needing a reference to the instance of the class, but only the type
     public static bool isFired;
 
@@ -14,6 +17,12 @@ public class Chain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(isFired)
+        {
+
+        } else{
+            // Move the chain based on the player position
+            transform.position = player.position;
+        }
     }
 }
