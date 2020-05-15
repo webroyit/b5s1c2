249d19fbead5cd0dc0c6b7d9a40e5abe56e81358
royweb <xytank@gmail.com>
2020-05-15 13:24:06 -0400

    Check if the chain hit the ball

--- Assets/Scripts/ChainCollision.cs
+++ Assets/Scripts/ChainCollision.cs
@@ -3,9 +3,17 @@
 public class ChainCollision : MonoBehaviour
 {
     // It is called when there is collision
-    void OnTriggerEnter2D()
+    void OnTriggerEnter2D(Collider2D col)
     {
         // Hide the chain
         Chain.IsFired = false;
+        
+        // You will need to set tag of the ball object in Unity to "Ball"
+        // Go to the ball object in Unity, click on tag dropdown menu, click "Add Tag...",
+        // then click the plus icon and enter the name of the tag
+        if(col.tag == "Ball")
+        {
+            Debug.Log("Split");
+        }
     }
 }
